0fba038f78cca8abb979d346fe8f5c2709fac32f
TKMhub <111867865+TKMhub@users.noreply.github.com>
2024-03-07 15:09:46 +0900

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import Image from "next/image";
 import { cookies } from "next/headers";
+import AuthButton from "./components/AuthButton";
 
 export default async function Home() {
   console.log(cookies());
@@ -8,5 +9,10 @@ export default async function Home() {
   const { data: posts } = await supabase.from("Posts").select();
   console.log("posts", posts);
 
-  return <pre>{JSON.stringify(posts, null, 2)}</pre>;
+  return (
+    <>
+      <AuthButton />
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
+    </>
+  );
 }
